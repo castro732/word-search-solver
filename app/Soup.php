50ac8284f910b3da78929b086fe94e063b7637ef
Jose <castro732@gmail.com>
2018-03-16 23:28:17 -0300

    Add constructor to the Soup model

--- app/Soup.php
+++ app/Soup.php
@@ -7,6 +7,52 @@ use Illuminate\Database\Eloquent\Model;
 class Soup extends Model
 {
 
+	protected $width;
+	protected $height;
+
+	function __construct($id) {
+
+		switch ($id) {
+    		case 1:
+    			$this->content =  [
+			        ['O','I','E'],
+			        ['I','I','X'],
+			        ['E','X','E'],
+			    ];
+
+			    $this->width;
+    			break;
+
+			case 2:
+				$this->content = [
+					['E','I','O','I','E','I','O','E','I','O']
+				];
+				break;
+
+			case 3:
+				$this->content = [
+					['E','A','E','A','E'],
+					['A','I','I','I','A'],
+					['E','I','O','I','E'],
+					['A','I','I','I','A'],
+					['E','A','E','A','E'],
+				];
+				break;
+
+			case 4:
+				$this->content = [
+					['O','X'],
+					['I','O'],
+					['E','X'],
+					['I','I'],
+					['O','X'],
+					['I','E'],
+					['E','X'],
+				];
+				break;
+
+    	$this->width = $this->soupWidth();
+	}
 }
 
 
