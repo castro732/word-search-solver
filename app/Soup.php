d4785694108290aa4a7418444245e8df9e406048
Jose <castro732@gmail.com>
2018-03-16 23:52:10 -0300

    Add horizontal and vertical search methods to Soup model

--- app/Soup.php
+++ app/Soup.php
@@ -68,6 +68,62 @@ class Soup extends Model
 		return count($this->content);
 	}
 	/**
+     * Search a given word horizontally in both directions (left to right, right to left)
+     * and returns how many times it was found.
+     *
+     * @param  str  $word
+     * @return int 
+     */
+    public function horizontalSearch($word)
+	{
+		$times_found = 0;
+
+		foreach ($this->content as $row) {
+			// Convert row to string
+			$row_str = implode($row);
+
+			// Perform the search in the string from left to right
+			$times_found += $this->countWordInString($word, $row_str);
+
+			// Reverse the row to perform search from right to left
+			$row = array_reverse($row);
+			$row_str = implode($row);
+			$times_found += $this->countWordInString($word, $row_str);
+		}
+		// Return the word found counter
+		return $times_found;
+	}
+
+	/**
+     * Search a given word vertically in both directions (top to bottom, bottom to top)
+     * and returns how many times it was found.
+     *
+     * @param  str  $word
+     * @return int 
+     */
+	public function verticalSearch($word)
+	{
+		$times_found = 0;
+
+		// Perform the search in columns
+		for ($i=0; $i < $this->width; $i++) { 
+			// Get column to perform search
+			$column = array_column($this->content, $i);
+			// Convert column to string
+			$column_str = implode($column);
+
+			// Perform the search in the string from top to bottom
+			$times_found += $this->countWordInString($word, $column_str);
+
+			// Reverse the column to perform search from bottom to top
+			$column = array_reverse($column);
+			$column_str = implode($column);
+			$times_found += $this->countWordInString($word, $column_str);
+
+		}
+		// Return the word found counter
+		return $times_found;
+	}
      * Counts how many times a given word appears in a given string
      * and returns how many times it was found.
      *
